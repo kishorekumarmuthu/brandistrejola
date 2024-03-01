316a45b2cd81064c18410ecb99e1df7fd8152494
Kishore Kumar <kishorekumarmuthu0906@gmail.com>
2024-03-01 19:59:17 +0530

    commit

---
+++ our-projects-section.php
@@ -0,0 +1,143 @@
+<!-- ======= Our Projects Section ======= -->
+<section id="projects" class="projects">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-header" style="padding-bottom: 30px;">
+            <h2>Our Projects</h2>
+        </div>
+        <div class="content" style="margin-bottom: 40px;">
+            <p>Step into our gallery and embark on a journey through three decades of exemplary craftsmanship and architectural marvels. Each exhibit tells a story of dedication, innovation, and unwavering commitment to excellence that defines Shan Associates.</p>
+            <p>Our gallery showcases a diverse portfolio of projects that have left an indelible mark on the landscape. Witness the seamless blend of commitment and functionality in every structure, reflecting our passion for construction and our relentless pursuit of perfection.</p>
+            <p>Every endeavor is a testament to our commitment to building dreams that stand the test of time.</p>
+        </div>
+
+        <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
+
+            <ul class="portfolio-flters" data-aos="fade-up" data-aos-delay="100">
+                <li data-filter="*" class="filter-active">All</li>
+                <li data-filter=".filter-remodeling">Remodeling</li>
+                <li data-filter=".filter-construction">Construction</li>
+                <li data-filter=".filter-repairs">Repairs</li>
+                <li data-filter=".filter-design">Design</li>
+            </ul><!-- End Projects Filters -->
+
+            <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/remodeling-1.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Remodeling 1</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/remodeling-1.jpg" title="Remodeling 1" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <?php include('./projects-section-construction.php') ?>
+
+                <!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/repairs-1.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Repairs 1</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/repairs-1.jpg" title="Repairs 1" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-design">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/design-1.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Design 1</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/design-1.jpg" title="Repairs 1" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/remodeling-2.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Remodeling 2</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/remodeling-2.jpg" title="Remodeling 2" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/repairs-2.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Repairs 2</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/repairs-2.jpg" title="Repairs 2" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-design">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/design-2.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Design 2</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/design-2.jpg" title="Repairs 2" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/remodeling-3.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Remodeling 3</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/remodeling-3.jpg" title="Remodeling 3" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/repairs-3.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Repairs 3</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/repairs-3.jpg" title="Repairs 2" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-design">
+                    <div class="portfolio-content h-100">
+                        <img src="assets/img/projects/design-3.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Design 3</h4>
+                            <p>Lorem ipsum, dolor sit amet consectetur</p>
+                            <a href="assets/img/projects/design-3.jpg" title="Repairs 3" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+            </div><!-- End Projects Container -->
+
+        </div>
+
+    </div>
+</section><!-- End Our Projects Section -->
---
+++ projects-section-construction.php
@@ -0,0 +1,165 @@
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image1.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 1</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image1.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image2.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 2</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image2.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image3.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 3</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image3.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image4.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 4</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image4.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image5.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 5</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image5.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image6.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 6</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image6.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image7.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 7</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image7.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image8.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 8</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image8.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image9.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 9</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image9.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image10.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 10</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image10.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image11.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 11</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image11.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image12.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 12</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image12.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image13.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 13</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image13.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image14.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 14</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image14.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 portfolio-item filter-construction">
+    <div class="portfolio-content h-100">
+        <img src="assets/img/projects/Buildings/image15.JPG" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Construction 15</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <a href="assets/img/projects/Buildings/image15.JPG" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+            <a href="project-details.php" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+        </div>
+    </div>
+</div>
